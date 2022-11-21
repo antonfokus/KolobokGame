e46d231330b72a602efc76177e683ea7f17c82cb
antonfokus <115660761+antonfokus@users.noreply.github.com>
2022-11-21 23:01:19 +0300

    Update ForceCursorVisible.cs

--- Lv1_3D/Scripts/ForceCursorVisible.cs
+++ Lv1_3D/Scripts/ForceCursorVisible.cs
@@ -1,5 +1,4 @@
-﻿// This code is part of the Fungus library (https://github.com/snozbot/fungus)
-// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
 
 using System.Collections;
 using System.Collections.Generic;
@@ -8,7 +7,7 @@ using UnityEngine;
 namespace Fungus.Examples
 {
     /// <summary>
-    /// Helper for FirstPerson Fungus Demo to work around some of the inflexibility of the Unity standard fps controller
+    /// Helper for FirstPerson Fungus to work around some of the inflexibility of the Unity standard fps controller
     /// </summary>
     public class ForceCursorVisible : MonoBehaviour
     {
